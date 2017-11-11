02ce48c9f5a1c6073c11f347a04a7dc8dbe3fc89
Anu Warrier <anuwarrier@ymail.com>
2017-11-11 17:20:07 +0530

    added figaro, did chmod, worried, and mailgun api

--- Gemfile
+++ Gemfile
@@ -35,7 +35,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 #Added by Anu
 gem 'devise'
-gem 'mailgun_rails'
+gem 'mailgun-ruby'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-        api_key: '<mailgun api key>',
-        domain: '<mailgun domain>'
+        api_key: ENV["mailgun_api_key"],
+        domain: ENV["mailgun_domain"]
   }
 
 
